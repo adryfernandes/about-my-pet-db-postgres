77e21791832962859d6cfce0c632ecfa7a325987
Adryane Fernandes <adryane.fernandes@inteli.edu.br>
2026-06-21 17:22:51 -0300

    feat: restructure user module exports and enhance UserRepository with type imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { PrismaClient } from "./generated/prisma/client";
 export { Sex } from "./generated/prisma/enums";
+export * from "./modules";
--- src/modules/user/userRepository.ts
+++ src/modules/user/userRepository.ts
@@ -1,16 +1,20 @@
-import { prisma } from "../../shared/database/prisma"
-import { CreateUserDTO } from "./userTypes"
+import { prisma } from "../../shared/database/prisma";
+import type { CreateUserDTO } from "./userTypes";
 
 export class UserRepository {
   async create(data: CreateUserDTO) {
-    return prisma.user.create({ data })
+    return prisma.user.create({ data });
+  }
+
+  async findAll() {
+    return prisma.user.findMany();
   }
 
   async findByEmail(email: string) {
-    return prisma.user.findUnique({ where: { email } })
+    return prisma.user.findUnique({ where: { email } });
   }
 
   async findByDocument(document: string) {
-    return prisma.user.findUnique({ where: { document } })
+    return prisma.user.findUnique({ where: { document } });
   }
 }
